e688dd8999c8f04f5ee80adfbdb8c227af5e5592
elaiya18 <e.raja2013@gmail.com>
2021-11-29 15:28:43 +0530

    employee

---
+++ src/org/tcs/test/Employee.java
@@ -0,0 +1,15 @@
+package org.tcs.test;
+
+public class Employee {
+	private void Id() {
+		System.out.println("Id is 4578");
+
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.Id();
+	}
+	
+	
+
+}
